1b1b0697cf044658c7343776e9168f8ef3f608f1
mdejan170 <dmandic@plume.com>
2023-01-05 02:17:31 +0100

    compact

--- src/toImage.py
+++ src/toImage.py
@@ -1,12 +1,5 @@
 from PIL import Image
 import numpy as np
-input = np.loadtxt("rezultati.txt", dtype='i')
-print(input)
 
-#data = np.zeros( (1024,1024,3), dtype=np.uint8 )
-
-#data[512,512] = [254,0,0]       # Makes the middle pixel red
-#data[512,513] = [0,0,255]       # Makes the next pixel blue
-input = (input * 55).astype(np.uint8)
-img = Image.fromarray(input)       # Create a PIL image
+img = Image.fromarray((np.loadtxt("rezultati.txt", dtype='i') * 55).astype(np.uint8)) 
 img.show()   
