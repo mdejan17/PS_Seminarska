5a189e71910e0437709967be33accd4060d54e44
mdejan170 <dmandic@plume.com>
2023-01-05 17:59:20 +0100

    moved functions to header

--- src/serial.c
+++ src/serial.c
@@ -23,37 +23,10 @@ int main(int argc, char **argv)
 
     // ustvari tabelo
     struct cell **field = (struct cell **)malloc(SIZE * sizeof(struct cell *));
-    for(int i = 0; i < SIZE; i++)
-        field[i] = (struct cell *)malloc(SIZE * sizeof(struct cell));
+    for(int i = 0; i < SIZE; i++) field[i] = (struct cell *)malloc(SIZE * sizeof(struct cell));
 
     //inicializiraj vrednosti
-    for(int i = 0; i < SIZE; i++)
-    {
-        for(int j = 0; j < SIZE; j++)
-        {
-            field[i][j].s = BETA;
-            if(field[i][j].type != 1)
-                field[i][j].type = 3;
-            init_n(field, i, j);
-            if(i == 0 || j == 0 || i == SIZE - 1 || j == SIZE - 1)
-            {
-                field[i][j].type = 2; // robna celica
-            }
-            else if(i == SIZE / 2 && j == SIZE / 2)
-            {
-                //srednja celica
-                field[i][j].s = 1.0;
-                field[i][j].type = 0;
-                for(int k = 0; k < 6; k++)
-                {
-                    //dovzetne celice
-                    int x = field[i][j].nx[k];
-                    int y = field[i][j].ny[k];
-                    field[x][y].type = 1;
-                }
-            }
-        }
-    }
+    init_table(field, SIZE, BETA);
 
     //naredi kopijo tabele
     struct cell **field2 = (struct cell **) malloc(SIZE * sizeof(struct cell *));
@@ -64,84 +37,30 @@ int main(int argc, char **argv)
     }
 
     //setaj up text file
-    char *filename = WRITE_FILE;
-    FILE *fp = fopen(filename, "w");
-    if (fp == NULL)
-    {
-        printf("Error opening the file %s", filename);
-        return -1;
-    }
+    FILE *fp = fopen(WRITE_FILE, "w");
 
-    while(!end(field, SIZE))
+    while(!end(field, SIZE)) // for every table 
     {
         //ponavljaj iteracije, dokler nismo na robu
         for(int i = 0; i < SIZE; i++)
-        {
             for(int j = 0; j < SIZE; j++)
-            {
-                //izracunaj koliko vode dobi celica
-                //robne celice imajo vedno BETA vode
-                if(field[i][j].type != 2)
-                {
-                    float sum = 0.0;
-                    for(int k = 0; k < 6; k++)
-                    {
-                        // (potencialni) sosedi
-                        int x = field[i][j].nx[k];
-                        int y = field[i][j].ny[k];
-                        if(!(x == -1 || y == -1 || x == SIZE || y == SIZE || field[x][y].type == 0 || field[x][y].type == 1))//to ni indeks veljavnega soseda ALI pa gre za dovzetno celico, ne naredi nič
-                            sum += field[x][y].s;
-                    }
-                    sum /= 6;
-                    if(field[i][j].type == 3)
-                        sum -= field[i][j].s;
-                    sum *= (ALFA/2);
-                    if(field[i][j].type == 0 || field[i][j].type == 1)
-                        sum += GAMA;
-                    field2[i][j].s += sum;
-                    if(field2[i][j].s >= 1 && field[i][j].type == 1)
-                    {
-                        //imamo progress
-                        progress = true;
-                        //celica zmrzne
-                        field2[i][j].type = 0;
-                        //sosedi zamrznjene celice postanejo dovzetni
-                        for(int k = 0; k < 6; k++)
-                        {
-                            int x = field[i][j].nx[k];
-                            int y = field[i][j].ny[k];
-                            if(x != -1 && y != -1 && x != SIZE && y != SIZE && field[x][y].type == 3)
-                                field2[x][y].type = 1;
-                        }
-                    }
-                } 
-            }
-        }
+                step(field, field2, &progress,  i,  j, SIZE, ALFA, GAMA);
 
         //pisi v file
-        if(progress)
-        {
-            progress = false;
-            for (int i = 0; i < SIZE; i++)
-            {
-                for (int j = 0; j < SIZE; j++)
-                    fprintf(fp, "%d ", field[i][j].type);
-                fprintf(fp, "\n");
-            }
-            fprintf(fp, "\n");
-        }
+        if(progress && !(progress = false))
+            write_to_file(fp, field, SIZE);
 
         //obdelali smo vse celice, samo se kopiramo tabele
         struct cell **temp = field;
-        field = field2;
-        field2 = temp;
-        for(int i = 0; i < SIZE; i++)
-            memcpy(field2[i], field[i], SIZE * sizeof(struct cell));
+        field = field2, field2 = temp;
+        for(int i = 0; i < SIZE; i++) memcpy(field2[i], field[i], SIZE * sizeof(struct cell));
     }
 
+    //merjenje casa
     endT = clock();
     cpu_time_used = ((double) (endT - start)) / CLOCKS_PER_SEC;
-    printf("cajt: %f\n", cpu_time_used);
+    printf("Cas izvajanja: %fs\n", cpu_time_used);
+    
     fclose(fp);
     free(field);
     free(field2);
--- src/serial.h
+++ src/serial.h
@@ -25,6 +25,100 @@ void init_n(struct cell **field, int i, int j)
     field[i][j].nx[5]  = (j % 2 == 1) ? i + 1 : i - 1;
 }
 
+void init_table(struct cell **field, int size, float beta)
+{
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            field[i][j].s = beta;
+            if(field[i][j].type != 1)
+                field[i][j].type = 3; //byfrost 
+            init_n(field, i, j);
+            if(i == 0 || j == 0 || i == size - 1 || j == size - 1)
+                field[i][j].type = 2; // robna celica
+            else if(i == size / 2 && j == size / 2)
+            {
+                //srednja celica
+                field[i][j].s = 1.0;
+                field[i][j].type = 0;
+                for(int k = 0; k < 6; k++)
+                {
+                    //mejne
+                    int x = field[i][j].nx[k];
+                    int y = field[i][j].ny[k];
+                    field[x][y].type = 1; 
+                }
+            }
+        }
+    }
+}
+
+void step(struct cell **field, struct cell **field2, bool *progress, int i, int j, int size, float alfa, float gama)
+{
+    //izracunaj koliko vode dobi celica
+    //robne celice imajo vedno BETA vode  
+    if(field[i][j].type != 2)
+    {
+        float sum = 0.0;
+        for(int k = 0; k < 6; k++)
+        {
+            // (potencialni) sosedi
+            int x = field[i][j].nx[k];
+            int y = field[i][j].ny[k];
+            if(!(x == -1 || y == -1 || x == size || y == size || field[x][y].type == 0 || field[x][y].type == 1))//to ni indeks veljavnega soseda ALI pa gre za dovzetno celico, ne naredi nič
+                sum += field[x][y].s;
+        }
+        sum /= 6;
+        if(field[i][j].type == 3)
+            sum -= field[i][j].s;
+        sum *= (alfa/2);
+        if(field[i][j].type == 0 || field[i][j].type == 1)
+            sum += gama;
+        field2[i][j].s += sum;
+        if(field2[i][j].s >= 1 && field[i][j].type == 1)
+        {
+            //imamo progress
+            *progress = true;
+            //celica zmrzne
+            field2[i][j].type = 0;
+            //sosedi zamrznjene celice postanejo dovzetni
+            for(int k = 0; k < 6; k++)
+            {
+                int x = field[i][j].nx[k];
+                int y = field[i][j].ny[k];
+                if(x != -1 && y != -1 && x != size && y != size && field[x][y].type == 3)
+                    field2[x][y].type = 1;
+            }
+        }
+    } 
+}
+
+
+
+void write_to_file(FILE *fp, struct cell **field, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+            fprintf(fp, "%d ", field[i][j].type);
+        fprintf(fp, "\n");
+    }
+    fprintf(fp, "\n");
+
+}
+
+bool end(struct cell **field, int size)
+{
+    //ce so zamrznjene celice prisle do roba, koncamo
+    for(int i = 1; i < size - 1; i++)
+        if(field[i][1].type == 0 || field[i][size - 2].type == 0) return true;
+    for(int j = 1; j < size - 1; j++)
+        if(field[1][j].type == 0 || field[size - 2][j].type == 0) return true;
+    return false;
+}
+
+// [DEBUG] not used
 void printTab(struct cell **field, int size)
 {
     for(int i = 0; i < size; i++)
@@ -56,14 +150,4 @@ void printHex(struct cell **field, int size)
         }
         printf("\n");
     }
-}
-
-bool end(struct cell **field, int size)
-{
-    //ce so zamrznjene celice prisle do roba, koncamo
-    for(int i = 1; i < size - 1; i++)
-        if(field[i][1].type == 0 || field[i][size - 2].type == 0) return true;
-    for(int j = 1; j < size - 1; j++)
-        if(field[1][j].type == 0 || field[size - 2][j].type == 0) return true;
-    return false;
 }
